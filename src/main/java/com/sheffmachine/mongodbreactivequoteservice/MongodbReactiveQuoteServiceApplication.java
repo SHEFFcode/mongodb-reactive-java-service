69f5f205b12d5ba278bf6c03e01c15c3518abc46
SHEFFmachine <sheff@sheffmachine.com>
2019-03-19 21:06:31 -0700

    Initial commit

---
+++ src/main/java/com/sheffmachine/mongodbreactivequoteservice/MongodbReactiveQuoteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sheffmachine.mongodbreactivequoteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbReactiveQuoteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbReactiveQuoteServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sheffmachine/mongodbreactivequoteservice/MongodbReactiveQuoteServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sheffmachine.mongodbreactivequoteservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MongodbReactiveQuoteServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
