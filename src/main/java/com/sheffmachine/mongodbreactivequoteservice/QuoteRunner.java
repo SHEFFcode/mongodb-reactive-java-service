3792f11d3ab443b90b09fab05177511b05b1b889
SHEFFmachine <sheff@sheffmachine.com>
2019-03-19 22:53:53 -0700

    Do the quote runner with findWithTailableCursor

--- src/main/java/com/sheffmachine/mongodbreactivequoteservice/QuoteRunner.java
+++ src/main/java/com/sheffmachine/mongodbreactivequoteservice/QuoteRunner.java
@@ -2,6 +2,7 @@ package com.sheffmachine.mongodbreactivequoteservice;
 
 import com.sheffmachine.mongodbreactivequoteservice.client.StockQuoteClient;
 import com.sheffmachine.mongodbreactivequoteservice.domain.Quote;
+import com.sheffmachine.mongodbreactivequoteservice.repositories.QuoteRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
@@ -9,14 +10,17 @@ import reactor.core.publisher.Flux;
 @Component
 public class QuoteRunner implements CommandLineRunner {
     private final StockQuoteClient stockQuoteClient;
+    private final QuoteRepository repository;
 
-    public QuoteRunner(StockQuoteClient stockQuoteClient) {
+    public QuoteRunner(StockQuoteClient stockQuoteClient, QuoteRepository repository) {
         this.stockQuoteClient = stockQuoteClient;
+        this.repository = repository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        Flux<Quote> quoteFlux = stockQuoteClient.getQuoteStream();
+//        Flux<Quote> quoteFlux = stockQuoteClient.getQuoteStream(); // do it one way
+        Flux<Quote> quoteFlux = repository.findWithTailableCursorBy();  // do it another way
 
         quoteFlux.subscribe(System.out::println);
     }
--- src/main/java/com/sheffmachine/mongodbreactivequoteservice/repositories/QuoteRepository.java
+++ src/main/java/com/sheffmachine/mongodbreactivequoteservice/repositories/QuoteRepository.java
@@ -2,6 +2,10 @@ package com.sheffmachine.mongodbreactivequoteservice.repositories;
 
 import com.sheffmachine.mongodbreactivequoteservice.domain.Quote;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+import reactor.core.publisher.Flux;
 
 public interface QuoteRepository extends ReactiveMongoRepository<Quote, String> {
+    @Tailable
+    Flux<Quote> findWithTailableCursorBy();
 }
